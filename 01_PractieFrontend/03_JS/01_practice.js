52fe3c17866a35755a46eb93b2d95bf85982a520
jojungon <jojungon@gmail.com>
2024-07-19 17:08:11 +0900

    js

---
+++ 01_PractieFrontend/03_JS/01_practice.js
@@ -0,0 +1,17 @@
+let number = 10
+number += 1
+number += 1
+console.log(number)
+console.log(number)
+console.log(number)
+
+let a = 10
+let b = '10'
+let d = '십'
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(d))
+
+const r = 10
+console.log(`원의 넓이는 ${3.14 * r * r}`)
+console.log(r++)
